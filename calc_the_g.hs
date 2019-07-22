ea2ebbe041808d4e91751b38503b8cc0ba92276a
杨竞颉 <j.yang19@ejm.org>
2019-07-22 23:52:28 +0800

    fix replace so that "_ => 00" is differed from "_ => 0"

--- calc_the_g.hs
+++ calc_the_g.hs
@@ -1,27 +1,27 @@
 type State = ((Int, Maybe Int), [Op])
 
-data Op = Add Int | Sub Int | Mul Int | Div Int | Insert Int |
-    Sum | Reverse | Replace Int Int | Delete | Shift | Inv10 |
-    Neg | Mirror | StoreNew | StoreInsert | IncrementMeta Int
+data Op = Add Int | Sub Int | Mul Int | Div Int | Insert Int | Neg |
+    Sum | Reverse | Replace String String | Delete | Shift | Inv10 |
+    Mirror | StoreNew | StoreInsert | IncrementFuckingBloodyMeta Int
 
 instance Show Op where
     show op = case op of
-        Add    n        -> "+ " ++ show n
-        Sub    n        -> "- " ++ show n
-        Mul    n        -> "* " ++ show n
-        Div    n        -> "/ " ++ show n
-        Insert n        -> show n
-        Sum             -> "SUM"
-        Reverse         -> "Reverse"
-        Replace a b     -> show a ++ " => " ++ show b
-        Delete          -> "<<"
-        Shift           -> "Shift >"
-        Inv10           -> "Inv10"
-        Neg             -> "+/-"
-        Mirror          -> "Mirror"
-        StoreNew        -> "Store"
-        StoreInsert     -> "Store"
-        IncrementMeta n -> "[+] " ++ show n
+        Add    n                     -> "+ " ++ show n
+        Sub    n                     -> "- " ++ show n
+        Mul    n                     -> "* " ++ show n
+        Div    n                     -> "/ " ++ show n
+        Insert n                     -> show n
+        Sum                          -> "SUM"
+        Reverse                      -> "Reverse"
+        Replace a b                  -> a ++ " => " ++ b
+        Delete                       -> "<<"
+        Shift                        -> "Shift >"
+        Inv10                        -> "Inv10"
+        Neg                          -> "+/-"
+        Mirror                       -> "Mirror"
+        StoreNew                     -> "Store"
+        StoreInsert                  -> "Store"
+        IncrementFuckingBloodyMeta n -> "[+] " ++ show n
 
 toDigits :: Int -> [Int]
 toDigits 0 = [0]
@@ -30,6 +30,9 @@ toDigits n | n < 0     = error "WTF this shouldn't happen"
   where
     toDigits' 0 = []
     toDigits' m = (m `mod` 10) : toDigits' (m `div` 10)
+
+digitsFromString :: String -> [Int]
+digitsFromString s = reverse $ map ((read :: String -> Int) . pure) s
 
 toInt :: [Int] -> Int
 toInt = foldr (\x y -> x + 10 * y) 0
@@ -40,24 +43,21 @@ replaceDigits ds old new = sub ds [] [] old
     sub bs res _   [] = sub bs (res ++ new) [] old
     sub [] res buf _  = res ++ buf
     sub (b : bs) res buf (c : cs)
-        | b == c    = sub bs res (buf ++ [b]) cs
-        | otherwise = sub bs (res ++ buf ++ [b]) [] old
+        | b == c        = sub bs res (buf ++ [b]) cs
+        | b == head old = sub bs (res ++ buf) [b] (tail old)
+        | otherwise     = sub bs (res ++ buf ++ [b]) [] old
 
-replaceInt :: Int -> Int -> Int -> Int
-replaceInt n a b = toInt $ reverse (replaceDigits n' a' b')
-  where
-    n' = reverse $ toDigits n
-    a' = reverse $ toDigits a
-    b' = reverse $ toDigits b
+replaceInt :: Int -> [Int] -> [Int] -> Int
+replaceInt n as bs = toInt $ reverse (replaceDigits n' as bs)
+    where n' = reverse $ toDigits n
 
 increment :: Int -> Op -> Op
-increment n (Add    m   ) = Add (m + n)
-increment n (Sub    m   ) = Sub (m + n)
-increment n (Mul    m   ) = Mul (m + n)
-increment n (Div    m   ) = Div (m + n)
-increment n (Insert m   ) = Insert (m + n)
-increment n (Replace a b) = Replace (a + n) (b + n)
-increment _ op            = op
+increment n (Add    m) = Add (m + n)
+increment n (Sub    m) = Sub (m + n)
+increment n (Mul    m) = Mul (m + n)
+increment n (Div    m) = Div (m + n)
+increment n (Insert m) = Insert (m + n)
+increment _ op         = op
 
 execute :: Op -> State -> [State]
 execute (Add m) ((n, mStore), ops) = [((n + m, mStore), ops)]
@@ -71,7 +71,11 @@ execute (Insert m) ((n, mStore), ops) =
 execute Sum ((n, mStore), ops) = [((sum $ toDigits n, mStore), ops)]
 execute Reverse ((n, mStore), ops) =
     [(((toInt . reverse . toDigits) n, mStore), ops)]
-execute (Replace a b) ((n, mStore), ops) = [((replaceInt n a b, mStore), ops)]
+execute (Replace a b) ((n, mStore), ops) =
+    [((replaceInt n as bs, mStore), ops)]
+  where
+    as = reverse $ digitsFromString a
+    bs = reverse $ digitsFromString b
 execute Delete ((n, mStore), ops) =
     [(((toInt . tail . toDigits) n, mStore), ops)]
 execute Shift ((n, mStore), ops) =
@@ -88,7 +92,7 @@ execute StoreNew    ((n, _     ), ops) = [((n, Just n), ops)]
 execute StoreInsert ((n, mStore), ops) = case mStore of
     Nothing -> []
     Just m  -> execute (Insert m) ((n, mStore), ops)
-execute (IncrementMeta m) ((n, mStore), ops) =
+execute (IncrementFuckingBloodyMeta m) ((n, mStore), ops) =
     [((n, mStore), fmap (increment m) ops)]
 
 eval :: (Int -> Int) -> [(State, [Op])] -> [(State, [Op])]
@@ -129,11 +133,15 @@ solution' v0 _ nbSteps ops f =
 
 main :: IO ()
 main =
-    let v0      = 0
-        goal    = -13
-        nbMoves = 4
-        ops     = [Add 3, Sub 7, Neg]
-        f       = id
+    let
+        v0      = 9
+        goal    = 3001
+        nbMoves = 9
+        ops =
+            [Replace "39" "93", Div 3, StoreNew, StoreInsert, Replace "31" "00"]
+        f   = id
         -- Change the parameters above according to the lvl
-        res     = solution v0 goal nbMoves ops f
-    in  print res
+        res = solution v0 goal nbMoves ops f
+    in
+        print res
+
