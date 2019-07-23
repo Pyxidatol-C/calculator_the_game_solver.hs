5de85e043312c27a550730d14ab81db632d30203
杨竞颉 <j.yang19@ejm.org>
2019-07-23 19:02:33 +0800

    refactor execute to handle negative numbers for certain ops

--- calc_the_g.hs
+++ calc_the_g.hs
@@ -1,17 +1,19 @@
+import           Data.Char                      ( isNumber )
+
 type State = ((Int, Maybe Int), [Op])
 
-data Op = Add Int | Sub Int | Mul Int | Div Int | Insert Int | Neg |
+data Op = Add Int | Sub Int | Mul Int | Div Int | Ins String | Neg |
     Sum | Reverse | Replace String String | Delete | Shift | Inv10 |
     Mirror | StoreNew | StoreInsert | IncrementFuckingBloodyMeta Int
     deriving (Eq)
 
 instance Show Op where
     show op = case op of
-        Add    n                     -> "+" ++ show n
-        Sub    n                     -> "-" ++ show n
-        Mul    n                     -> "*" ++ show n
-        Div    n                     -> "/" ++ show n
-        Insert n                     -> show n
+        Add n                        -> "+" ++ show n
+        Sub n                        -> "-" ++ show n
+        Mul n                        -> "*" ++ show n
+        Div n                        -> "/" ++ show n
+        Ins n                        -> n
         Sum                          -> "SUM"
         Reverse                      -> "Reverse"
         Replace a b                  -> a ++ " => " ++ b
@@ -20,20 +22,24 @@ instance Show Op where
         Inv10                        -> "Inv10"
         Neg                          -> "+/-"
         Mirror                       -> "Mirror"
-        StoreNew                     -> "Store"
-        StoreInsert                  -> "Store"
+        StoreNew                     -> "Store (new)"
+        StoreInsert                  -> "Store (Ins)"
         IncrementFuckingBloodyMeta n -> "[+]" ++ show n
 
 toDigits :: Int -> [Int]
 toDigits 0 = [0]
-toDigits n | n < 0     = error "WTF this shouldn't happen"
-           | otherwise = toDigits' n
+toDigits n
+    | n < 0 = error "Internal: attempted to geet digits of a negative number?!"
+    | otherwise = toDigits' n
   where
     toDigits' 0 = []
     toDigits' m = (m `mod` 10) : toDigits' (m `div` 10)
 
-digitsFromString :: String -> [Int]
-digitsFromString s = reverse $ map ((read :: String -> Int) . pure) s
+stringToDigits :: String -> [Int]
+stringToDigits s = reverse $ map ((read :: String -> Int) . pure) s
+
+digitsToString :: [Int] -> String
+digitsToString = map (head . show)
 
 toInt :: [Int] -> Int
 toInt = foldr (\x y -> x + 10 * y) 0
@@ -49,16 +55,40 @@ replaceDigits ds old new = sub ds [] [] old
         | otherwise     = sub bs (res ++ buf ++ [b]) [] old
 
 replaceInt :: Int -> [Int] -> [Int] -> Int
-replaceInt n as bs = toInt $ reverse (replaceDigits n' as bs)
-    where n' = reverse $ toDigits n
+replaceInt n as bs = toInt $ reverse (replaceDigits n' as' bs')
+  where
+    n'  = reverse $ toDigits n
+    as' = reverse as
+    bs' = reverse bs
 
 increment :: Int -> Op -> Op
-increment n (Add    m) = Add (m + n)
-increment n (Sub    m) = Sub (m + n)
-increment n (Mul    m) = Mul (m + n)
-increment n (Div    m) = Div (m + n)
-increment n (Insert m) = Insert (m + n)
-increment _ op         = op
+increment n (Add m) = Add (m + n)
+increment n (Sub m) = Sub (m + n)
+increment n (Mul m) = Mul (m + n)
+increment n (Div m) = Div (m + n)
+increment n (Ins m) = Ins
+    (digitsToString . normaliseDigits $ addDigits (stringToDigits m)
+                                                  (toDigits n)
+    )
+increment _ op = op
+
+addDigits :: [Int] -> [Int] -> [Int]
+addDigits xs ys | length xs < length ys = addDigits ys xs
+                | otherwise             = zipWith (+) xs (ys ++ repeat 0)
+
+normaliseDigits :: [Int] -> [Int]
+normaliseDigits ds = normaliseDigits' ds 0 []
+
+normaliseDigits' :: [Int] -> Int -> [Int] -> [Int]
+normaliseDigits' [] 0 acc = acc
+normaliseDigits' [] c acc = normaliseDigits' [c] 0 acc
+normaliseDigits' (d : ds) c acc =
+    let s = d + c in normaliseDigits' ds (s `div` 10) (s `mod` 10 : acc)
+
+
+executeNeg :: Op -> State -> [State]
+executeNeg op ((n, mStore), ops) =
+    execute op ((-n, mStore), ops) >>= execute Neg
 
 execute :: Op -> State -> [State]
 execute (Add m) ((n, mStore), ops) = [((n + m, mStore), ops)]
@@ -67,32 +97,61 @@ execute (Mul m) ((n, mStore), ops) = [((n * m, mStore), ops)]
 execute (Div m) ((n, mStore), ops)
     | n `mod` m == 0 = [((n `div` m, mStore), ops)]
     | otherwise      = []
-execute (Insert m) ((n, mStore), ops) =
-    [((toInt (toDigits m ++ toDigits n), mStore), ops)]
-execute Sum ((n, mStore), ops) = [((sum $ toDigits n, mStore), ops)]
-execute Reverse ((n, mStore), ops) =
-    [(((toInt . reverse . toDigits) n, mStore), ops)]
-execute (Replace a b) ((n, mStore), ops) =
-    [((replaceInt n as bs, mStore), ops)]
+
+execute op@(Ins m) st@((n, mStore), ops)
+    | not $ all isNumber m = error "You can only `Ins`ert a positive integer!"
+    | n < 0 = executeNeg op st
+    | otherwise = [((toInt (stringToDigits m ++ toDigits n), mStore), ops)]
+
+execute op@Sum st@((n, mStore), ops)
+    | n < 0     = executeNeg op st
+    | otherwise = [((sum $ toDigits n, mStore), ops)]
+
+execute op@Reverse st@((n, mStore), ops)
+    | n < 0     = executeNeg op st
+    | otherwise = [(((toInt . reverse . toDigits) n, mStore), ops)]
+
+execute op@(Replace a b) st@((n, mStore), ops)
+    | not $ all isNumber a = error "You can only replace a positive integer!"
+    | not $ all isNumber b = error "You can only replace by a positive integer!"
+    | n < 0                = executeNeg op st
+    | otherwise            = [((replaceInt n as bs, mStore), ops)]
   where
-    as = reverse $ digitsFromString a
-    bs = reverse $ digitsFromString b
-execute Delete ((n, mStore), ops) =
-    [(((toInt . tail . toDigits) n, mStore), ops)]
-execute Shift ((n, mStore), ops) =
-    let digits = toDigits n
-    in  [((toInt (tail digits ++ [head digits]), mStore), ops)]
-execute Inv10 ((n, mStore), ops) = [((toInt (map f digits), mStore), ops)]
+    as = stringToDigits a
+    bs = stringToDigits b
+
+execute op@Delete st@((n, mStore), ops)
+    | n < 0     = executeNeg op st
+    | otherwise = [(((toInt . tail . toDigits) n, mStore), ops)]
+
+execute op@Shift st@((n, mStore), ops)
+    | n < 0
+    = executeNeg op st
+    | otherwise
+    = let digits = toDigits n
+      in  [((toInt (tail digits ++ [head digits]), mStore), ops)]
+
+execute op@Inv10 st@((n, mStore), ops)
+    | n < 0     = executeNeg op st
+    | otherwise = [((toInt (map f digits), mStore), ops)]
   where
     digits = toDigits n
     f x = (10 - x) `mod` 10
+
 execute Neg ((n, mStore), ops) = [((-n, mStore), ops)]
-execute Mirror ((n, mStore), ops) =
-    let ds = toDigits n in [((toInt (reverse ds ++ ds), mStore), ops)]
+
+execute op@Mirror st@((n, mStore), ops)
+    | n < 0
+    = executeNeg op st
+    | otherwise
+    = let ds = toDigits n in [((toInt (reverse ds ++ ds), mStore), ops)]
+
 execute StoreNew    ((n, _     ), ops) = [((n, Just n), ops)]
+
 execute StoreInsert ((n, mStore), ops) = case mStore of
     Nothing -> []
-    Just m  -> execute (Insert m) ((n, mStore), ops)
+    Just m  -> execute (Ins $ show m) ((n, mStore), ops)
+
 execute (IncrementFuckingBloodyMeta m) ((n, mStore), ops) =
     [((n, mStore), fmap (increment m) ops)]
 
@@ -140,7 +199,7 @@ main =
     let v0      = 25
         goal    = 822
         nbMoves = 6
-        ops     = [Mirror, Insert 5, StoreNew, StoreInsert, Delete]
+        ops     = [Mirror, Ins "5", StoreNew, StoreInsert, Delete]
         f       = portal 3 1
         -- Change the parameters above according to the lvl
         res     = solution v0 goal nbMoves ops f
